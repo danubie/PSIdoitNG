e4c8ad31837152b6c11a791f8a59df647a1b5885
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2025-05-27 19:11:12 +0200

    add func Remove-IdoitCatagory

---
+++ src/Public/Remove-IdoitCategory.ps1
@@ -0,0 +1,58 @@
+function Remove-IdoitCategory {
+    <#
+    .SYNOPSIS
+    Remove an i-doit object category entry.
+
+    .DESCRIPTION
+    This function removes an entry from a category of an object. Currently the API only supports removal of entries of multi-value categories.
+
+    .PARAMETER Id
+    The ID of the i-doit object.
+
+    .PARAMETER Category
+    The category of the i-doit object from which the entry should be removed.
+
+    .PARAMETER EntryId
+    The ID of the entry to be removed from the category.
+
+    .EXAMPLE
+    Remove-IdoitCategory -Id 12345 -Category 'C__CATG__MEMORY' -EntryId 12
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ObjId')]
+        [int] $Id,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Category,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateRange(1, [int]::MaxValue)]
+        [int] $EntryId
+    )
+
+    begin {
+
+    }
+
+    process {
+        if (-not $PSCmdlet.ShouldProcess("Idoit Object with ID $Id in category '$Category'", "Remove")) {
+            Write-Verbose "Skipping removal of object with ID $Id in category '$Category' due to ShouldProcess."
+            return
+        }
+        $params = @{
+            objID = $Id
+            category = $Category
+            id = $EntryId           # I hate this API
+        }
+        $apiEndpoint = 'cmdb.category.delete'
+        $apiResult = Invoke-IdoIt -Endpoint $apiEndpoint -Params $params
+        Write-Output $apiResult
+    }
+
+    end {
+
+    }
+}
