bfd43e2389f9a691ecd8be1ec0088fade4d42a4c
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2025-07-13 06:30:40 +0200

    add cleanup for Jane&John Pester

--- tests/Integration/Helpers/Remove-PesterLeftOvers.ps1
+++ tests/Integration/Helpers/Remove-PesterLeftOvers.ps1
@@ -1,5 +1,6 @@
 # cleanup leftovers from a previous run
-$obj = Search-IdoItObject -Conditions @{"property" = "C__CATG__GLOBAL-title"; "comparison" = "like"; "value" = "Pester*"} -ErrorAction SilentlyContinue
+# usually object titles start with Pester; If it it a PERSON object, it will have a FirstName and LastName as title
+$obj = Search-IdoItObject -Conditions @{"property" = "C__CATG__GLOBAL-title"; "comparison" = "like"; "value" = "*Pester*"} -ErrorAction SilentlyContinue
 $obj | Foreach-Object {
     Write-Verbose "Removing leftover object with Id: $($_.ObjId) and title: $($_.Title)"
     Remove-IdoitObject -ObjId $_.ObjId -Method Purge
